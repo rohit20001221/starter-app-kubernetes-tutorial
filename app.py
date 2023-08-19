cb0b1a4667222d797bc7da72287f45adc33eb8e4
rohit20001221 <rohit20001221@gmail.com>
2023-08-19 21:14:04 +0530

    .

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from calc.funs import add
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html"), 200
+
+@app.route("/<int:a>/<int:b>")
+def index(a, b):
+    return render_template("index.html", a=a, b=b, c=add(a, b)), 200
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 8000))
--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,2 @@
 docker build . -t flask-starter
 docker tag flask-starter:latest rohit20001221/flask-starter
-docker push rohit20001221/flask-starter
