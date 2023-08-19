c0f3c28399856a2626ec8f95b362a2ae67524d7a
rohit20001221 <rohit20001221@gmail.com>
2023-08-20 01:03:42 +0530

    .

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,7 +20,7 @@ pipeline {
             }
         }
         
-        stage('Build the Docker file') {
+        stage('Build the Docker file.') {
             steps {
                 sh "./build-docker.sh"    
             }
